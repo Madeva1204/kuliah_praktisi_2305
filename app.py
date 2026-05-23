86b7c4e3d2c328eb96ca7440cbc6cfc1507b5ef4
Madeva1204 <mayangdevanidwinanda@gmail.com>
2026-05-23 10:30:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,8 @@ st.caption("Made with ❤️ using Streamlit")
 
 title = st.text_input("Movie title", "Life of Brian")
 st.write("The current movie title is", title)
+
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
